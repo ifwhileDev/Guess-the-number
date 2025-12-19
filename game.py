5ceb117358d3267c899249ab01b3c5c06f15acc5
Matheus Raymundo <92649949+M-Raymundo@users.noreply.github.com>
2025-12-19 18:29:16 -0300

    Update game.py

--- game.py
+++ game.py
@@ -1,12 +1,21 @@
 from random import randint
 
-number = randint(0, 10)
+pc = randint(0, 10)
 
-player = int(input('Choose a number between 0 and 10: '))
+print("-=" * 10)
+print("GUESS THE NUMBER")
+print("-=" * 10)
 
-if player == number:
-    print('CONGRATS!! YOU WON!')
+player = int(input('Choose a number between 0 & 10: '))
 
-else:
-    print(f'The CP choice were {number}. Hope you get better luck next time!')
-  
+while player != pc:
+    if player > 10 or player < 0:
+        print("Invalid number! Try again.")
+    elif player > pc:
+        print("Your number is too high, choose another one.")
+    elif player < pc:
+        print("Your number is too low, choose another one.")
+
+    player = int(input('Choose a number: '))
+
+print(f"CONGRATS!!! The number was {pc}. YOU WON.")
